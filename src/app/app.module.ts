1d803e27d9885e410f174de2a9d3e043bf6ae14b
Thilini <thiliniomattage@outlook.com>
2020-09-20 20:28:36 -0700

    added nav bar footer and home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AppRoutingModule } from './app-routing-module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './component/nav/nav.component';
+import {RouterModule} from '@angular/router';
+import { FooterComponent } from './component/footer/footer.component';
+import { HomeComponent } from './component/home/home.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
